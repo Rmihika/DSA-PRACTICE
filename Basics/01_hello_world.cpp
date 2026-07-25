11dd67c80109511b4d091b12915290229b651096
Mihika <mihikaraut29@gmail.com>
2026-07-25 21:32:54 +0530

    array prac

--- Basics/01_hello_world.cpp
+++ Basics/01_hello_world.cpp
@@ -3,4 +3,9 @@ using namespace std;
 int main()
 {
     cout << "hello world" << endl;
+    int array[5] = {13, 33, 55, 89};
+    cout << "enter value for 5th posn:" << endl;
+    cin >> array[4];
+    cout << "value entered is:" << array[4];
+    cout << "entire array:" << array;
 }
