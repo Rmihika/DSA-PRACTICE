d24e38e273d7b61799ba7befb2c3eefbe1c38c4f
Mihika <mihikaraut29@gmail.com>
2026-08-10 23:08:07 +0530

    patternd

--- Basics/Patterns/EmptySqaure.cpp
+++ Basics/Patterns/EmptySqaure.cpp
@@ -27,3 +27,33 @@ int main()
 
     return 0;
 }
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 4;
+    for (int row = 1; row <= n; row++)
+    {
+        // for each row -> 6 columns
+        for (int col = 1; col <= 6; col++)
+        {
+            if (row == 1 || row == n)
+            {
+                cout << "*";
+            }
+            else if (col == 1 || col == 6)
+            {
+                cout << "*";
+            }
+
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
