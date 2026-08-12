eaf2d59979342842802e9aae3122471580413474
Mihika <mihikaraut29@gmail.com>
2026-08-12 17:02:31 +0530

    patterns

---
+++ Basics/Patterns/HollowPyramid.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 5;
+    for (int row = 1; row <= n; row++)
+    {
+        for (int col = 1; col <= n - row; col++)
+        {
+            cout << "  ";
+        }
+        if (row == 1 || row == n)
+        {
+            for (int col = 1; col <= (2 * n - row); col++)
+            {
+                cout << "* ";
+            }
+        }
+        else
+        {
+            cout << "*";
+            for (int col = 1; col <= 2 * row - 3; col++)
+            {
+                cout << "  ";
+            }
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
---
+++ Basics/Patterns/HollowrightAngleTriangle.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 5;
+    for (int row = 1; row <= n; row++)
+    {
+        if (row == 1 || row == 2 || row == 5)
+        {
+            for (int col = 1; col <= row; col++)
+            {
+                cout << "* ";
+            }
+        }
+        else
+        {
+            cout << "*";
+            for (int col = 1; col <= (row - 2); col++)
+            {
+                cout << "  ";
+            }
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
